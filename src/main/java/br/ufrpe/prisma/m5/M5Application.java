8c61e6848608787a63ee9445353494277deaf316
Láila Lemos <samaralailaa@gmail.com>
2026-06-29 15:29:37 +0000

    inicializando spring boot

---
+++ src/main/java/br/ufrpe/prisma/m5/M5Application.java
@@ -0,0 +1,11 @@
+package br.ufrpe.prisma.m5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(M5Application.class, args);
+    }
+}
---
+++ src/test/java/br/ufrpe/prisma/m5/M5ApplicationTests.java
@@ -0,0 +1,14 @@
+package br.ufrpe.prisma.m5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class M5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
